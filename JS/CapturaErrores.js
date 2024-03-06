ad6304f9745d4f5078b16e2398cbf2c74cfd4672
LCYTAdrianNo1 <80604147+LCYTAdrianNo1@users.noreply.github.com>
2024-03-05 19:58:55 -0500

    V1.0.050324
    -Se añade que imprima los errores en consola

---
+++ JS/CapturaErrores.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    try {
+        
+    } catch (error) {
+        // Registra el error en la consola
+        console.error('Se produjo un error:', error);
+    }
+});
