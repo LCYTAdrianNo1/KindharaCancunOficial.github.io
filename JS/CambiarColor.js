75fc76216fb0fd5772477ee93527ab481c0a18b2
LCYTAdrianNo1 <80604147+LCYTAdrianNo1@users.noreply.github.com>
2024-04-07 17:06:36 -0500

    V1.1.070424
    -Se añadio para cambiar el texto del boton dependiendo del modo seleccionado

--- JS/CambiarColor.js
+++ JS/CambiarColor.js
@@ -1,13 +1,16 @@
 function cambiarModo() {
     var cuerpo = document.body;
+    var modoBoton = document.getElementById("modoBoton");
     // Verifica si el cuerpo tiene la clase 'modo-claro'
     if (cuerpo.classList.contains("modo-claro")) {
-        // Si está en modo claro, cambia a modo oscuro
+        // Si está en modo claro, cambia a modo oscuro y cambia el texto del botón a ☀️
         cuerpo.classList.remove("modo-claro");
         cuerpo.classList.add("modo-oscuro");
+        modoBoton.textContent = "☀️";
     } else {
-        // Si está en modo oscuro, cambia a modo claro
+        // Si está en modo oscuro, cambia a modo claro y cambia el texto del botón a 🌙
         cuerpo.classList.remove("modo-oscuro");
         cuerpo.classList.add("modo-claro");
+        modoBoton.textContent = "🌙";
     }
 }
