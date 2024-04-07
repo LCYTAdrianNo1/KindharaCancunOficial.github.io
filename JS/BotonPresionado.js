4b756fc5f62accd69716f089f389f5586b5213b7
LCYTAdrianNo1 <80604147+LCYTAdrianNo1@users.noreply.github.com>
2024-04-07 17:18:26 -0500

    V1.0.070424
    -Se añadio el registro de boton presionado

---
+++ JS/BotonPresionado.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let installButton = document.getElementById('installButton');
+
+    installButton.addEventListener('click', function() {
+        console.log('El botón de instalación ha sido presionado');
+    });
+});
