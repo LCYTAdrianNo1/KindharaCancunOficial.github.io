718f2cd3ab7a9385fd4b72168d7c82e8fd652547
LCYTAdrianNo1 <80604147+LCYTAdrianNo1@users.noreply.github.com>
2024-04-07 20:39:41 -0500

    V1.1.070424
    -Se añadio la correccion de la direccion

--- JS/DireccionGPS.js
+++ JS/DireccionGPS.js
@@ -1,11 +1,9 @@
 function openGoogleMaps() {
     // Coordenadas de destino
-    var lat = 21.1195184;
-    var lng = -86.8730271;
-
+    var lat = 21.119406;
+    var lng = -86.871926;   
     // Construir la URL de Google Maps con las coordenadas
     var url = 'https://www.google.com/maps/dir//' + lat + ',' + lng;
-
     // Abrir Google Maps en una nueva ventana
     window.open(url);
 }
